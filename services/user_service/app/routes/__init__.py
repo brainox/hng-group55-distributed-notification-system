c024d83c85f37868fa0d1f1a14fa7dd2594fd719
Zeehy123 <Zeehy123@users.noreply.github.com>
2025-11-12 10:09:56 +0100

    "Removed preferences_routes import from __init__.py"

--- services/user_service/app/routes/__init__.py
+++ services/user_service/app/routes/__init__.py
@@ -1,4 +1,3 @@
 from .user_routes import router as user_router
 from .health_routes import router as health_router
 from .auth_routes import router as auth_router
-from .preferences_routes import router as preferences_router
